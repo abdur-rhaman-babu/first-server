6494b8f25ac37546cb06d4276088952ee2fde47a
abdur rhaman babu <abdurrahmanbab74@gamil.com>
2024-11-24 21:02:27 +0600

    fetch data

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require('express')
 const app = express()
+var cors = require('cors')
 const port = process.env.PORT || 5000;
 
+app.use(cors())
+
+const users = [
+    {id:1, name:'Sakib', email:'sakib@gamil.com'},
+    {id:2, name:'Tamim', email:'tamim@gamil.com'},
+    {id:3, name:'Mushfiq', email:'mushfiq@gamil.com'}
+]
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World Bangladesh!')
+})
+
+app.get('/users', (req, res) => {
+  res.send(users)
 })
 
 app.listen(port, () => {
